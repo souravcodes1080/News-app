2cf1a0356215847846a1c5e0cbee97ad08fae13b
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-10-08 00:22:16 +0530

    added comments in code for better readability

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -4,23 +4,34 @@ import { URL } from "../../../config";
 import axios from "axios";
 import Card from "./Card/Card";
 import loader from "../../../public/loader.gif";
+
 function MainSection() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     fetchNews();
   }, []);
 
+  //function to fetch news data from API
   const fetchNews = async () => {
     setLoading(true);
-    const response = await await axios.get(URL);
-    if (response.data.status === "ok") {
-      setNews(response.data.articles);
-      setLoading(false);
-    } else {
-      setLoading(false);
+    setError(null); 
+    try {
+      const response = await axios.get(URL);
+      if (response.data.status === "ok") {
+        setNews(response.data.articles);
+      } else {
+        throw new Error("Failed to fetch news articles");
+      }
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false); 
     }
   };
+
   return (
     <>
       <div className="title">
@@ -28,25 +39,25 @@ function MainSection() {
       </div>
       {loading ? (
         <div className="loader">
-          <img src={loader}  alt="loading..."/>
+          <img src={loader} alt="loading..." />
+        </div>
+      ) : error ? (
+        <div className="error-message">
+          <p>{error}</p> 
         </div>
       ) : (
-        <>
-          <div className="card-grid">
-            {news.map((n, index) => {
-              return (
-                <Card
-                  key={index}
-                  title={n.title}
-                  urlToImage={n.urlToImage}
-                  publishedAt={n.publishedAt}
-                  url={n.url}
-                  description={n.description}
-                />
-              );
-            })}
-          </div>
-        </>
+        <div className="card-grid">
+          {news.map((n, index) => (
+            <Card
+              key={index}
+              title={n.title}
+              urlToImage={n.urlToImage}
+              publishedAt={n.publishedAt}
+              url={n.url}
+              description={n.description}
+            />
+          ))}
+        </div>
       )}
     </>
   );
