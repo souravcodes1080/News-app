bb8af3f187e50bd9c6243cbc94f822f7732255d9
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2025-05-18 22:11:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import MainSection from "./components/MainSection/MainSection";
 import Footer from "./components/Footer/Footer";
-import './app.css'
+import './App.css'
 function App() {
   return (
     <>
